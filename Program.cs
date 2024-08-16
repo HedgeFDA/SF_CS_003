7f0ff5d35b98aadeca9e3abe94a4bd8b92aee57c
Ilya Boiko <boiko.ilya.evg@gmail.com>
2024-08-16 18:21:13 +0500

    3.3.2

--- Program.cs
+++ Program.cs
@@ -4,11 +4,20 @@ class Program
 {
     public static void Main(string[] args)
     {
-        string myName;
-        myName = "Jane";
+        string Name = "Jane";
+        byte Age    = 27;
+        bool Pet    = true;
+        float Size  = 37.5F;
 
-        Console.WriteLine(myName);
-
-        Console.ReadKey();
+        Console.WriteLine("My name is " + Name);
+        Console.WriteLine("My age is " + Age);
+        if (Pet) {
+            Console.WriteLine("I have a pet");
+        }
+        else
+        {
+            Console.WriteLine("I don't have a pet");
+        }
+        Console.WriteLine("My shoe size is " + Size);
     }
 }
