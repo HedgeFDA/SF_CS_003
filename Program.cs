3b80b63ec76cc1c7005de7f9dd2ab12ea47f815b
Ilya Boiko <boiko.ilya.evg@gmail.com>
2024-08-16 17:50:30 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string myName;
+        myName = "Jane";
+
+        Console.WriteLine(myName);
+
+        Console.ReadKey();
+    }
+}
